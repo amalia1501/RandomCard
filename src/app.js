554b8dd10a51f8f02e8f2eddfc8d7a1eb6d20350
amalia1501 <amibottini@hotmail.com>
2023-03-21 02:25:17 +0000

    Random Card Base

--- src/app.js
+++ src/app.js
@@ -8,4 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+  let simbol = ["♦", "♥", "♠", "♣"];
+  let randomNumber = Math.floor(Math.random() * number.length);
+  let randomSimbol = Math.floor(Math.random() * simbol.length);
+  let simbolTop = document.querySelector(".top");
+  simbolTop.innerHTML = simbol[randomSimbol];
+  let simbolBottom = document.querySelector(".bottom");
+  simbolBottom.innerHTML = simbol[randomSimbol];
+  let numberCenter = document.querySelector(".number");
+  numberCenter.innerHTML = number[randomNumber];
+
+  if (simbol[randomSimbol] === "♥" || simbol[randomSimbol] === "♦") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  } else {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+  }
 };
